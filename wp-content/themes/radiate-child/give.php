b4a4049e8a70d58c9a12b38fb45636c0d80b5c9c
stanislav-chechun <stanislav@primary.technology>
2015-12-28 18:07:14 +0200

    first stage to display content

--- wp-content/themes/radiate-child/give.php
+++ wp-content/themes/radiate-child/give.php
@@ -12,8 +12,9 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
 
-get_header(); //echo "sssssNNNNNRR fefefef";
-$ss = get_post_meta(176);
+get_header();
+$form_id = 176;
+$ss = get_post_meta($form_id);
 echo '<pre>';
 //var_dump($ss);
 echo '</pre>';
@@ -27,7 +28,8 @@ do_action( 'give_before_main_content' );
 while ( have_posts() ) : the_post();
 
 	//give_get_template_part( 'single-give-form/content', 'single-give-form' );
-$form_content = get_post_meta(176, '_give_form_content');
+//$form_content = get_post_meta($id_form, '_give_form_content');
+$content = wpautop( get_post_meta( $form_id, '_give_form_content', true ) );
 //echo $form_content['0']; ?>
 <article id="post-<?php the_ID(); ?>" <?php //post_class(); ?>>
     <div class="row">
@@ -35,12 +37,19 @@ $form_content = get_post_meta(176, '_give_form_content');
             <iframe width="500" height="400" src="https://www.youtube.com/embed/RGcr9KG1m8I?rel=0" frameborder="0" rel="0" allowfullscreen></iframe>
         </div>
         <div class="col-md-6" id="post-text">
+            <h2><?php the_title(); ?></h2>
 
             <?php// give_get_template_part( 'single-give-form/content', 'single-give-form' );?>
-<?php give_get_donation_form( $args = array() ); ?>
-            <?php echo give_get_users_completed_donations(); ?>
+            <?php give_get_donation_form( $args = array() ); ?>
+            <?php give_get_d ?>
         </div>
         </div><!-- #div row-## --> 
+        
+        <div class="row">
+            <div class="col-md-12" id="text-content">
+               <div id="give-form-content-<?php echo $form_id ?>"><?php echo $content; ?> </div>
+            </div>
+        </div>
 </article><!-- #post-## --> <?php
 endwhile; // end of the loop.
 
