7d07da552fa7c684ee6fbdc292e72d571753dabb
Stan Chechun <stanislav@primary.technology>
2016-01-04 18:31:09 +0200

    change for Student Profile page

---
+++ wp-content/themes/radiate-child/tamplate-student-profile.php
@@ -0,0 +1,48 @@
+<?php
+/*
+Template Name: Student Profile
+*/
+get_header(); ?>
+
+<div class="content-wrap text-page">
+		
+	<?php 
+		
+		$id_user = $_GET['id_user'];
+
+	 	if (isset($id_user)){
+			
+			$user_meta = get_user_meta($id_user); 
+			$user_info = get_userdata($id_user);
+			$user_image_from_profile = wp_get_attachment_image( $user_meta['photo_37'][0], 'medium' );
+			$user_video_from_profile = wp_get_attachment_url($user_meta['video_42'][0] );
+
+			//if (isset($user_video_from_profile)){
+			if ($user_video_from_profile != ''){
+
+	?>
+
+				<video controls="controls">
+					<source src="<?php echo $user_video_from_profile; ?>">
+				</video>
+
+	<?php
+			}
+			elseif ($user_image_from_profile != ''){
+				echo $user_image_from_profile;
+			}
+			else{
+				echo get_avatar($id_user);
+			}	 			 
+
+			echo '<h2>' . $user_info->first_name .' '. $user_info->last_name . '</h2>'; 
+			echo '<p>' . $user_info->description . '</p>';			
+			echo '<p>' . $user_meta['story_54'][0] . '</p>';
+
+		}	 	
+
+	?>	
+
+</div>
+
+<?php get_footer(); ?>
