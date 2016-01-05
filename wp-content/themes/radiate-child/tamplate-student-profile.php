56ce732e2d3eb3825f3eb556be903963c43d0030
Stan Chechun <stanislav@primary.technology>
2016-01-05 14:26:37 +0200

    add student wishes

--- wp-content/themes/radiate-child/tamplate-student-profile.php
+++ wp-content/themes/radiate-child/tamplate-student-profile.php
@@ -16,43 +16,70 @@ get_header(); ?>
 			$user_info = get_userdata($id_user);
 			$user_image_from_profile = wp_get_attachment_image( $user_meta['photo_37'][0], 'medium' );
 			$user_video_from_profile = wp_get_attachment_url($user_meta['video_42'][0] );
+			$user_login = $user_info->user_nicename;
 
 	?>
 
-	<div class="for-media">
+			<div class="for-media">
 
 	<?php
 
-			//if (isset($user_video_from_profile)){ 
-			if ($user_video_from_profile != ''){
+				if ($user_video_from_profile != ''){
 
 	?>
-
-				<video controls="controls">
-					<source src="<?php echo $user_video_from_profile; ?>">
-				</video>
-
+					<video controls="controls">
+						<source src="<?php echo $user_video_from_profile; ?>">
+					</video>
 	<?php
-			}
-			elseif ($user_image_from_profile != ''){
-				echo $user_image_from_profile;
-			}
-			else{
-				echo get_avatar($id_user);
-			}
-
+				}
+				elseif ($user_image_from_profile != ''){
+					echo $user_image_from_profile;
+				}
+				else{
+					echo get_avatar($id_user);
+				}
 	?>
-
-	</div>
+			</div>
 
 	<?php	 			 
 
 			echo '<h2>' . $user_info->first_name .' '. $user_info->last_name . '</h2>'; 
 			echo '<p>' . $user_info->description . '</p>';			
-			echo '<p>' . $user_meta['story_54'][0] . '</p>';
+			echo '<p>' . $user_meta['story_54'][0] . '</p>';	
 
-		}	 	
+		}	
 
+		$args = array(
+			'post_type' => 'give_forms', 
+			'meta_key' => 'autor_login', 
+			'meta_value' => $user_login,
+		);
+
+		$posts = new WP_Query( $args );
+
+		if ( $posts->have_posts() ) {
+	?>
+
+			<div class="row">
+
+	<?php			
+				while ( $posts->have_posts() ) {
+					$posts->the_post();
+	?>
+
+					<div>
+						<h2 class="wsite-content-title"><?php the_title(); ?></h2>
+						<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>				
+					</div>
+
+	<?php }	?>
+
+			</div>
+			
+	<?php			
+		} 
+		wp_reset_postdata(); 	
+	
 	?>	
 
 </div>
