acbfc8b68ee92c889964fa1b5c92e499ce14cbd5
Stan Chechun <stanislav@primary.technology>
2016-01-06 11:29:26 +0200

    add links to slides in carousel wishes

--- wp-content/themes/radiate-child/tamplate-student-profile.php
+++ wp-content/themes/radiate-child/tamplate-student-profile.php
@@ -72,8 +72,8 @@ get_header(); ?>
 					
 							<div class="item">			      
 						    	<?php if ( has_post_thumbnail() ) { the_post_thumbnail(); } ?>
-						    	<div class="carousel-caption">
-						        	<?php the_title(); ?>
+						    	<div class="carousel-caption">						 
+						        	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 						    	</div>
 						    </div>
 					    
