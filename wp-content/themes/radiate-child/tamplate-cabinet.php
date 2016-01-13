e01d6b7985ecc891e2f150761e8f2694be61ce82
Stan Chechun <stanislav@primary.technology>
2016-01-13 13:25:29 +0200

    add tamplate for cabinet

---
+++ wp-content/themes/radiate-child/tamplate-cabinet.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Template Name: Cabinet
+*/
+get_header(); ?>
+
+<div class="content-wrap text-page">
+		
+	<?php 
+		while (have_posts()) : the_post();		
+			the_content(); 			
+	 	
+		 	$user = $_GET['user'];
+		 	$user_info = get_userdata($user);
+			echo $user_info->roles[0];
+
+			/*if ( $user_info->roles[0] == 'student' ){
+				//echo "this is student";
+				//rcl_activate_addon('meet-mentor');
+				//rcl_deactivate_addon('meet-mentor');
+			}
+
+			if ( $user_info->roles[0] == 'mentor' ){
+				//echo "this is student";
+				//rcl_deactivate_addon('meet-mentor');
+				//<span class="rcl-tab-button" data-tab="meet-mentor" id="tab-button-meet-mentor"><a href="http://localhost/campus-rize/student-member-login/?user=2&amp;tab=meet-mentor" class="recall-button block_button"><i class="fa fa-calendar"></i><span>Meet your Mentor</span></a></span>
+			}	*/	 
+		 	
+	 	endwhile;
+
+	?>	
+
+</div>
+
+<?php get_footer(); ?>
