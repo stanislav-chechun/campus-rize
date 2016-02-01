68427c47905330a567d5579ec04fc5e4fc4f8e95
Stan Chechun <stanislav@primary.technology>
2016-02-01 20:10:10 +0200

    fix for Land Your Dream Job page

--- wp-content/wp-recall/add-on/land-your-dream-job/index.php
+++ wp-content/wp-recall/add-on/land-your-dream-job/index.php
@@ -61,8 +61,8 @@ add_action('init','add_tab_land_your_dream_job');
 								   	<td>'. $goal_give[0].'</td>
 								</tr>';*/
 
-					$profession .= '<div class="row">
-										<div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">';
+					$profession .= '<div class="row profession">
+										<div class="col-lg-2 col-md-2 col-sm-2 col-xs-12">';
 
 					if ( has_post_thumbnail() ) { 
 						//$profession .= the_post_thumbnail(); 
@@ -72,10 +72,14 @@ add_action('init','add_tab_land_your_dream_job');
 					}
 
 					$profession .= '</div>							        
-									<div class="col-lg-10 col-md-10 col-sm-10 col-xs-6">
-										<h2>'.get_the_title().'</h2>
+									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+										<h2><a href="'.get_the_permalink().'">'.get_the_title().'</a></h2>
+									</div>
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+										<p>Industry: '.get_post_meta($post_id, "prof_industry", 1).'</p>
+										<p>Location: '.get_post_meta($post_id, "prof_location", 1).'</p>
 									</div>									
-									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
+									<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
 										<p><i class="fa fa-envelope-o"></i>'.get_post_meta($post_id, "prof_email", 1).'</p>
 										<p><i class="fa fa-phone"></i>'.get_post_meta($post_id, "prof_tel", 1).'</p>
 									</div>
