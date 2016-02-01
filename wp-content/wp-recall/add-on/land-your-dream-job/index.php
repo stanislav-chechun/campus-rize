54dabc8e7569c69ba7857c48b6202c9ea342ac74
Stan Chechun <stanislav@primary.technology>
2016-02-01 19:13:11 +0200

    template for Land Your Dream Job page

---
+++ wp-content/wp-recall/add-on/land-your-dream-job/index.php
@@ -0,0 +1,105 @@
+<?php
+
+add_action('init','add_tab_land_your_dream_job');
+
+	function add_tab_land_your_dream_job(){
+
+		$user = $_GET['user'];
+		$user_info = get_userdata($user);
+
+		if ( $user_info->roles[0] == 'student' ){		
+	    	rcl_tab('dream_job', 'land_your_dream_job_showup', 'Land Your Dream Job', array('public'=>0,'class'=>'fa-briefcase','order'=>11));
+	    }
+	}
+
+	function land_your_dream_job_showup(){
+		//$user = $_GET['user'];		
+
+	 	//if (isset($user)){			
+			
+			//$user_info = get_userdata($user);			
+			//$user_login = $user_info->user_nicename;
+
+			$args = array(
+				'post_type' => 'professions', 
+			);
+
+			$posts = new WP_Query( $args );
+
+			if ( $posts->have_posts() ) {
+
+				//$goal_sum = 0;												
+				//$earnings_sum = 0;
+
+				/*$awards .= '<div class="your-awards">
+								<h3>Award Summary</h3>
+								<p>The following reflects awarded aid. Refer to your Award Notification e-mail for additional details.</p>
+								<table>
+									<tr>
+									    <th>Award Description</th>
+									    <th>Category</th>
+									    <th>Offered, $</th>
+									    <th>Accepted, $</th>
+								   	</tr>';*/
+
+				while ( $posts->have_posts() ) {
+					$posts->the_post();
+					$post_id = get_the_ID();
+					//$post_id = get_the_ID();
+					//$goal_give = get_post_meta($post_id, '_give_set_goal');												
+					//$earnings = get_post_meta($post_id, '_give_form_earnings');
+
+					//$goal = str_replace(",", "", $goal_give);
+									
+					//$goal_sum += $goal[0];
+					//$earnings_sum += $earnings[0];
+
+					/*$awards .= '<tr>
+								   	<td><a href="'. get_the_permalink() .'">' . get_the_title().'</a></td>
+								   	<td>wishes</td>
+								   	<td>'. $earnings[0]. '</td>
+								   	<td>'. $goal_give[0].'</td>
+								</tr>';*/
+
+					$profession .= '<div class="row">
+										<div class="col-lg-2 col-md-2 col-sm-2 col-xs-6">';
+
+					if ( has_post_thumbnail() ) { 
+						//$profession .= the_post_thumbnail(); 
+					} 
+					else {
+						$profession .='<img src="'.get_home_url().'/wp-content/uploads/2016/01/no_photo.png" alt="">';
+					}
+
+					$profession .= '</div>							        
+									<div class="col-lg-10 col-md-10 col-sm-10 col-xs-6">
+										<h2>'.get_the_title().'</h2>
+									</div>									
+									<div class="col-lg-4 col-md-4 col-sm-4 col-xs-6">
+										<p><i class="fa fa-envelope-o"></i>'.get_post_meta($post_id, "prof_email", 1).'</p>
+										<p><i class="fa fa-phone"></i>'.get_post_meta($post_id, "prof_tel", 1).'</p>
+									</div>
+									</div>';
+									
+
+				}
+
+				/*$awards .= '<tr class="totals">
+						   		<td colspan="2">Totals:</td>
+						   		<td>'. $earnings_sum .'</td>
+						   		<td>'. $goal_sum .'</td>
+						   	</tr>
+						</table>
+						<a href="#" class="cabinet-btn">Redeem Awards</a>
+						<a href="#" class="cabinet-btn">Transfer Funds</a>
+					</div>';
+
+				return $awards;*/
+				return $profession;
+			}
+			
+			wp_reset_postdata();
+		//}    
+	}
+
+?>
