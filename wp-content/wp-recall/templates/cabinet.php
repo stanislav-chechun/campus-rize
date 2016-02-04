ba65917de3740135e65b494fc52e599dfda61b68
Stan Chechun <stanislav@primary.technology>
2016-02-04 18:33:25 +0200

    styles for cabinet

--- wp-content/wp-recall/templates/cabinet.php
+++ wp-content/wp-recall/templates/cabinet.php
@@ -9,21 +9,21 @@
         <div class="lk-header rcl-node">
             <?php rcl_header(); ?>
         </div>
-        <div class="lk-sidebar">
+        <!--<div class="lk-sidebar">
             <div class="lk-avatar">
-                <?php rcl_avatar(120); ?>
+                <?php //rcl_avatar(120); ?>
             </div>
             <div class="rcl-node">
                 <?php rcl_sidebar(); ?>
             </div>
-        </div>
-        <div class="lk-content">
+        </div>-->
+        <!--<div class="lk-content">
             <h2><?php rcl_username(); ?>
             </h2>
             <div class="rcl-action">
                 <?php rcl_action(); ?>
             </div>
-            <p class="text-muted"><?php $user = $_GET['user']; $user_info = get_userdata($user); echo $user_info->roles[0]; ?></p>
+            <p class="text-muted"><?php //$user = $_GET['user']; $user_info = get_userdata($user); echo $user_info->roles[0]; ?></p>
             <div class="rcl-user-status">
                 <?php rcl_status_desc(); ?>
             </div>
@@ -33,13 +33,40 @@
             <div class="lk-footer rcl-node">
                 <?php rcl_footer(); ?>
             </div>
-        </div>
+        </div>-->
 
     </div>
 
     <?php $class = (isset($rcl_options['buttons_place'])&&$rcl_options['buttons_place']==1)? "left-buttons":""; ?>
     <div id="rcl-tabs">
         <div id="lk-menu" class="rcl-menu <?php echo $class; ?> rcl-node">
+
+            <div class="lk-sidebar">
+                <div class="lk-avatar">
+                    <?php rcl_avatar(120); ?>
+                </div>
+                <div class="rcl-node">
+                    <?php rcl_sidebar(); ?>
+                </div>
+            </div>
+            <div class="lk-content">
+                <h2><?php rcl_username(); ?>
+                </h2>
+                <div class="rcl-action">
+                    <?php rcl_action(); ?>
+                </div>
+                <p class="text-muted"><?php $user = $_GET['user']; $user_info = get_userdata($user); echo $user_info->roles[0]; ?></p>
+                <div class="rcl-user-status">
+                    <?php rcl_status_desc(); ?>
+                </div>
+                <div class="rcl-content">
+                    <?php rcl_content(); ?>
+                </div>
+                <div class="lk-footer rcl-node">
+                    <?php rcl_footer(); ?>
+                </div>
+            </div>
+
             <?php rcl_buttons(); ?>
         </div>
         <div id="chat-inner-tabs">
