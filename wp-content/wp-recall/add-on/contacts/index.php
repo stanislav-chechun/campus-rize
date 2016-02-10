671b6c0b780b42de6a108e9cf5b1a390add467f6
Stan Chechun <stanislav@primary.technology>
2016-02-10 17:58:56 +0200

    styles for the cabinet

--- wp-content/wp-recall/add-on/contacts/index.php
+++ wp-content/wp-recall/add-on/contacts/index.php
@@ -17,7 +17,7 @@ add_action('init','add_tab_chat_with_students');
 		$user = $_GET['user'];
 		$user_info = get_userdata($user);		
 		
-		return do_shortcode("[userlist inpage='10' orderby='display_name' order='ASC' exclude=$user_info->ID filters='0' search_form='0']");
+		return do_shortcode("[userlist template='avatars' inpage='10' orderby='display_name' order='ASC' exclude=$user_info->ID filters='0' search_form='0']");
 	 	
 	}
 
